327a18452899249bfe2569db4a5bde39383f8560
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-04 01:55:28 +0300

    Add Seminar 6 Task 41

---
+++ Cons_App_006 Seminar 6 Task 41/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте,сколько чисел больше 0 ввёл пользователь.:
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("=============== Семинар 6 Задача 41 =================");
+            Console.Write("Введите количество проверяемых чисел: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            int count = 0;
+            for (int i = 0; i < index; i++)
+            {
+                Console.Write("Введите число " + (i + 1) + " = ");
+                int number = Convert.ToInt32(Console.ReadLine());
+                if (number > 0) count += 1;
+            }
+            Console.WriteLine("Количество введенных чисел > 0 равно: " + count);
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
