501e09d18b8c250c822d545c4322e9441750f98a
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-04 23:23:02 +0300

    Add edit Time

--- Cons_App_008 Time/Program.cs
+++ Cons_App_008 Time/Program.cs
@@ -1,50 +1,62 @@
-﻿DateTime date1 = DateTime.Now;
-string time = Convert.ToString(date1);
-for (int i = 11; i < 19; i++) Console.Write(time[i]);
-Console.WriteLine("");
-
-void Print(char[,] timearray, int index)// Печать массива
-{
-    for (int i = 0; i < 5; i++)
+﻿
+    void Print(char[,] timearray, int index)// Печать массива
     {
-        for (int j = 0; j < 3; j++)
+        for (int i = 0; i < 5; i++)
         {
-            Console.SetCursorPosition(j + index * 5, i + 3);
-            Console.Write(timearray[i, j]);
+            for (int j = 0; j < 3; j++)
+            {
+                Console.SetCursorPosition(j + index * 5, i + 3);
+                Console.Write(timearray[i, j]);
+            }
+            Console.WriteLine("");
         }
-        Console.WriteLine("");
     }
-}
 
-char[,] null_array = new char[5, 3] { { '*', '*', '*' }, { '*', ' ', '*' }, { '*', ' ', '*' }, { '*', ' ', '*' }, { '*', '*', '*' } };
-char[,] one_array = new char[5, 3] { { ' ', ' ', '*' }, { ' ', '*', '*' }, { '*', ' ', '*' }, { ' ', ' ', '*' }, { ' ', ' ', '*' } };
-char[,] two_array = new char[5, 3] { { '*', '*', '*' }, { ' ', ' ', '*' }, { ' ', ' ', '*' }, { ' ', '*', ' ' }, { '*', '*', '*' } };
-char[,] three_array = new char[5, 3] { { '*', '*', '*' }, { ' ', ' ', '*' }, { '*', '*', '*' }, { ' ', ' ', '*' }, { '*', '*', '*' } };
-char[,] four_array = new char[5, 3] { { '*', ' ', '*' }, { '*', ' ', '*' }, { '*', '*', '*' }, { ' ', ' ', '*' }, { ' ', ' ', '*' } };
-char[,] five_array = new char[5, 3] { { '*', '*', '*' }, { '*', ' ', ' ' }, { '*', '*', '*' }, { ' ', ' ', '*' }, { '*', '*', '*' } };
-char[,] six_array = new char[5, 3] { { '*', '*', '*' }, { '*', ' ', ' ' }, { '*', '*', '*' }, { '*', ' ', '*' }, { '*', '*', '*' } };
-char[,] seven_array = new char[5, 3] { { '*', '*', '*' }, { ' ', ' ', '*' }, { ' ', '*', ' ' }, { '*', ' ', ' ' }, { '*', ' ', ' ' } };
-char[,] eight_array = new char[5, 3] { { '*', '*', '*' }, { '*', ' ', '*' }, { '*', '*', '*' }, { '*', ' ', '*' }, { '*', '*', '*' } };
-char[,] nine_array = new char[5, 3] { { '*', '*', '*' }, { '*', ' ', '*' }, { '*', '*', '*' }, { ' ', ' ', '*' }, { '*', '*', '*' } };
-char[,] point_array = new char[5, 3] { { ' ', ' ', ' ' }, { ' ', '*', ' ' }, { ' ', ' ', ' ' }, { ' ', '*', ' ' }, { ' ', ' ', ' ' } };
+    char[,] null_array = new char[5, 3] { { '0', '0', '0' }, { '0', ' ', '0' }, { '0', ' ', '0' }, { '0', ' ', '0' }, { '0', '0', '0' } };
+    char[,] one_array = new char[5, 3] { { ' ', ' ', '0' }, { ' ', '0', '0' }, { '0', ' ', '0' }, { ' ', ' ', '0' }, { ' ', ' ', '0' } };
+    char[,] two_array = new char[5, 3] { { '0', '0', '0' }, { ' ', ' ', '0' }, { ' ', ' ', '0' }, { ' ', '0', ' ' }, { '0', '0', '0' } };
+    char[,] three_array = new char[5, 3] { { '0', '0', '0' }, { ' ', ' ', '0' }, { '0', '0', '0' }, { ' ', ' ', '0' }, { '0', '0', '0' } };
+    char[,] four_array = new char[5, 3] { { '0', ' ', '0' }, { '0', ' ', '0' }, { '0', '0', '0' }, { ' ', ' ', '0' }, { ' ', ' ', '0' } };
+    char[,] five_array = new char[5, 3] { { '0', '0', '0' }, { '0', ' ', ' ' }, { '0', '0', '0' }, { ' ', ' ', '0' }, { '0', '0', '0' } };
+    char[,] six_array = new char[5, 3] { { '0', '0', '0' }, { '0', ' ', ' ' }, { '0', '0', '0' }, { '0', ' ', '0' }, { '0', '0', '0' } };
+    char[,] seven_array = new char[5, 3] { { '0', '0', '0' }, { ' ', ' ', '0' }, { ' ', '0', ' ' }, { '0', ' ', ' ' }, { '0', ' ', ' ' } };
+    char[,] eight_array = new char[5, 3] { { '0', '0', '0' }, { '0', ' ', '0' }, { '0', '0', '0' }, { '0', ' ', '0' }, { '0', '0', '0' } };
+    char[,] nine_array = new char[5, 3] { { '0', '0', '0' }, { '0', ' ', '0' }, { '0', '0', '0' }, { ' ', ' ', '0' }, { '0', '0', '0' } };
+    char[,] point_array = new char[5, 3] { { ' ', ' ', ' ' }, { ' ', '0', ' ' }, { ' ', ' ', ' ' }, { ' ', '0', ' ' }, { ' ', ' ', ' ' } };
 
-int index_number = 0;
-for (int i = 11; i < 19; i++)
+    void Time()
 {
+    DateTime date1 = DateTime.Now;
+    string time = Convert.ToString(date1);
 
-    if (time[i] == '0') Print(null_array, index_number);
-    if (time[i] == '1') Print(one_array, index_number);
-    if (time[i] == '2') Print(two_array, index_number);
-    if (time[i] == '3') Print(three_array, index_number);
-    if (time[i] == '4') Print(four_array, index_number);
-    if (time[i] == '5') Print(five_array, index_number);
-    if (time[i] == '6') Print(six_array, index_number);
-    if (time[i] == '7') Print(seven_array, index_number);
-    if (time[i] == '8') Print(eight_array, index_number);
-    if (time[i] == '9') Print(nine_array, index_number);
-    if (time[i] == ':') Print(point_array, index_number);
-    index_number++;
+    int index_number = 0;
+    for (int i = 11; i < 19; i++)
+    {
+
+        if (time[i] == '0') Print(null_array, index_number);
+        if (time[i] == '1') Print(one_array, index_number);
+        if (time[i] == '2') Print(two_array, index_number);
+        if (time[i] == '3') Print(three_array, index_number);
+        if (time[i] == '4') Print(four_array, index_number);
+        if (time[i] == '5') Print(five_array, index_number);
+        if (time[i] == '6') Print(six_array, index_number);
+        if (time[i] == '7') Print(seven_array, index_number);
+        if (time[i] == '8') Print(eight_array, index_number);
+        if (time[i] == '9') Print(nine_array, index_number);
+        if (time[i] == ':') Print(point_array, index_number);
+        index_number++;
+        Console.WriteLine("");
+        Console.WriteLine("");
+
+    }
+        
+        TimeSpan interval = new TimeSpan(0, 0, 1);
+        TimeSpan interval2 = date1 - DateTime.Now;
+        
+        Thread.Sleep(interval - interval2);
+        Console.Clear();
+        Time();
 }
-Console.WriteLine("");
+Time();
 
 
--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,39 +1,14 @@
-﻿DateTime date1 = DateTime.Now;
-Console.WriteLine(date1.ToString("T"));
-string time = Convert.ToString(date1);
-for (int i = 11; i < 19; i++) Console.Write(time[i]);
-Console.WriteLine("");
-void Print(char[,] timearray)// Печать массива
+﻿void Time()
 {
-    for (int i = 0; i < 5; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            Console.Write(timearray[i, j]);
-        }
-        Console.WriteLine("");
-    }
+    DateTime date1 = DateTime.Now;
+    string time = Convert.ToString(date1);
+    Console.WriteLine("");
+    for (int i = 11; i < 19; i++) Console.Write(time[i]);
+    Console.WriteLine("");
+    Console.WriteLine("");
+    
+    Thread.Sleep(1000);
+    Console.Clear();
+    Time();
 }
-
-char [,] null_array = new char[5,3] {{'*', '*', '*'},{'*', ' ', '*'}, {'*', ' ', '*'}, {'*', ' ', '*'}, {'*', '*', '*'}};
-char [,] one_array = new char[5,3] {{' ', ' ', '*'},{' ', '*', '*'}, {'*', ' ', '*'}, {' ', ' ', '*'}, {' ', ' ', '*'}};
-char [,] two_array = new char[5,3] {{'*', '*', '*'},{' ', ' ', '*'}, {' ', ' ', '*'}, {' ', '*', ' '}, {'*', '*', '*'}};
-char [,] three_array = new char[5,3] {{'*', '*', '*'},{' ', ' ', '*'}, {'*', '*', '*'}, {' ', ' ', '*'}, {'*', '*', '*'}};
-char [,] four_array = new char[5,3] {{'*', ' ', '*'},{'*', ' ', '*'}, {'*', '*', '*'}, {' ', ' ', '*'}, {' ', ' ', '*'}};
-char [,] five_array = new char[5,3] {{'*', '*', '*'},{'*', ' ', ' '}, {'*', '*', '*'}, {' ', ' ', '*'}, {'*', '*', '*'}};
-char [,] six_array = new char[5,3] {{'*', '*', '*'},{'*', ' ', ' '}, {'*', '*', '*'}, {'*', ' ', '*'}, {'*', '*', '*'}};
-char [,] seven_array = new char[5,3] {{'*', '*', '*'},{' ', ' ', '*'}, {' ', '*', ' '}, {'*', ' ', ' '}, {'*', ' ', ' '}};
-char [,] eight_array = new char[5,3] {{'*', '*', '*'},{'*', ' ', '*'}, {'*', '*', '*'}, {'*', ' ', '*'}, {'*', '*', '*'}};
-char [,] nine_array = new char[5,3] {{'*', '*', '*'},{'*', ' ', '*'}, {'*', '*', '*'}, {' ', ' ', '*'}, {'*', '*', '*'}};
-
-Print(null_array);
-Print(one_array);
-Print(two_array);
-Print(three_array);
-Print(four_array);
-Print(five_array);
-Print(six_array);
-Print(seven_array);
-Print(eight_array);
-Print(nine_array);
-
+Time();
