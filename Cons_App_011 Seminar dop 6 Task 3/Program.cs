dc71fa2d4348bdf26aac6cba0c2c1df4af7bf7bf
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-05 20:07:00 +0300

    Add Seminar dop 6 Task 3

---
+++ Cons_App_011 Seminar dop 6 Task 3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число N: ");
+double a = Convert.ToDouble(Console.ReadLine());
+double n = 3;
+
+double stepen = 0;
+double step = 0;
+
+double Step(double result)
+{
+    while (a >= n)
+    {
+        a = a / 3;
+        stepen = stepen + 1;
+        Console.WriteLine(a + ", " + stepen);
+        step = stepen;
+        Step(a);
+    }
+return step;
+}
+
+Console.WriteLine(Step(a));
