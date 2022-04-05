26c5d3295fabb4deb5934ae807b46ca0e826e195
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-05 11:55:42 +0300

    Add Seminar 6 task 1 dop

--- Cons_App_008 Seminar dop 6 Task 4/Program.cs
+++ Cons_App_008 Seminar dop 6 Task 4/Program.cs
@@ -1,62 +1,74 @@
-﻿
-void Print(char[,] timearray, int index)// Печать массива
+﻿// Задача 1* доп Семинар 6. Создайте программу, показывающую текущее время.
+using System;
+
+namespace ConsoleApp
 {
-    for (int i = 0; i < 5; i++)
+    class Program
     {
-        for (int j = 0; j < 5; j++)
+        static void Main(string[] args)
         {
-            Console.SetCursorPosition(j + index * 7, i + 3);
-            Console.Write(timearray[i, j]);
+
+            void Print(char[,] timearray, int index)// Печать массива
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        Console.SetCursorPosition(j + index * 7, i + 3);
+                        Console.Write(timearray[i, j]);
+                    }
+                    Console.WriteLine("");
+                }
+            }
+            // цифры символами
+            char[,] null_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] one_array = new char[5, 5] { { ' ', ' ', '0', ' ', ' ' }, { ' ', '0', '0', ' ', ' ' }, { '0', ' ', '0', ' ', ' ' }, { ' ', ' ', '0', ' ', ' ' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] two_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', ' ' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] three_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] four_array = new char[5, 5] { { '0', ' ', ' ', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' } };
+            char[,] five_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', ' ' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] six_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', ' ' }, { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] seven_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', '0', ' ' }, { ' ', ' ', '0', ' ', ' ' }, { ' ', ' ', '0', ' ', ' ' }, { ' ', ' ', '0', ' ', ' ' } };
+            char[,] eight_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] nine_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
+            char[,] point_array = new char[5, 5] { { ' ', ' ', ' ', ' ', ' ' }, { ' ', ' ', 'O', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ' }, { ' ', ' ', 'O', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ' } };
+
+            // вывод времени
+            void Time()
+            {
+                DateTime date1 = DateTime.Now;
+                string time = Convert.ToString(date1);
+
+                int index_number = 0;
+                for (int i = 11; i < 19; i++)
+                {
+
+                    if (time[i] == '0') Print(null_array, index_number);
+                    if (time[i] == '1') Print(one_array, index_number);
+                    if (time[i] == '2') Print(two_array, index_number);
+                    if (time[i] == '3') Print(three_array, index_number);
+                    if (time[i] == '4') Print(four_array, index_number);
+                    if (time[i] == '5') Print(five_array, index_number);
+                    if (time[i] == '6') Print(six_array, index_number);
+                    if (time[i] == '7') Print(seven_array, index_number);
+                    if (time[i] == '8') Print(eight_array, index_number);
+                    if (time[i] == '9') Print(nine_array, index_number);
+                    if (time[i] == ':') Print(point_array, index_number);
+                    index_number++;
+                    Console.WriteLine("");
+                    Console.WriteLine("");
+                }
+
+                TimeSpan interval = new TimeSpan(0, 0, 1);
+                TimeSpan interval2 = date1 - DateTime.Now;
+
+                Thread.Sleep(interval - interval2);
+                Console.Clear();
+                Time();
+            }
+            Time();
         }
-        Console.WriteLine("");
     }
 }
-// цифры символами
-char[,] null_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
-char[,] one_array = new char[5, 5] { { ' ', ' ', '0', ' ', ' ' }, { ' ', '0', '0', ' ', ' ' }, { '0', ' ', '0', ' ', ' ' }, { ' ', ' ', '0', ' ', ' ' }, { '0', ' ', '0', ' ', '0' } };
-char[,] two_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', ' ' }, { '0', ' ', '0', ' ', '0' } };
-char[,] three_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
-char[,] four_array = new char[5, 5] { { '0', ' ', ' ', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' } };
-char[,] five_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', ' ' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
-char[,] six_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', ' ' }, { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
-char[,] seven_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', '0', ' ' }, { ' ', ' ', '0', ' ', ' ' }, { ' ', ' ', '0', ' ', ' ' }, { ' ', ' ', '0', ' ', ' ' } };
-char[,] eight_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
-char[,] nine_array = new char[5, 5] { { '0', ' ', '0', ' ', '0' }, { '0', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' }, { ' ', ' ', ' ', ' ', '0' }, { '0', ' ', '0', ' ', '0' } };
-char[,] point_array = new char[5, 5] { { ' ', ' ', ' ', ' ', ' ' }, { ' ', ' ', 'O', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ' }, { ' ', ' ', 'O', ' ', ' ' }, { ' ', ' ', ' ', ' ', ' ' } };
-
-// вывод времени
-void Time()
-{
-    DateTime date1 = DateTime.Now;
-    string time = Convert.ToString(date1);
-
-    int index_number = 0;
-    for (int i = 11; i < 19; i++)
-    {
-
-        if (time[i] == '0') Print(null_array, index_number);
-        if (time[i] == '1') Print(one_array, index_number);
-        if (time[i] == '2') Print(two_array, index_number);
-        if (time[i] == '3') Print(three_array, index_number);
-        if (time[i] == '4') Print(four_array, index_number);
-        if (time[i] == '5') Print(five_array, index_number);
-        if (time[i] == '6') Print(six_array, index_number);
-        if (time[i] == '7') Print(seven_array, index_number);
-        if (time[i] == '8') Print(eight_array, index_number);
-        if (time[i] == '9') Print(nine_array, index_number);
-        if (time[i] == ':') Print(point_array, index_number);
-        index_number++;
-        Console.WriteLine("");
-        Console.WriteLine("");
-    }
-
-    TimeSpan interval = new TimeSpan(0, 0, 1);
-    TimeSpan interval2 = date1 - DateTime.Now;
-
-    Thread.Sleep(interval - interval2);
-    Console.Clear();
-    Time();
-}
-Time();
 
 
---
+++ Cons_App_009 Seminar dop 6 Task 1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 1 доп Семинар 6. Написать перевод десятичного числа в двоичное, используя рекурсию
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 6 Задача 1 доп =============");
+            Console.Write("Введите число в десятичной системе счисления: ");
+            int dvoid = Convert.ToInt32(Console.ReadLine());
+            string text = string.Empty;
+            string Perevod(int number)
+            {
+                if (number >= 1)
+                {
+                    text = number % 2 + text;
+                    Perevod(number/2);
+                }
+                return text;
+            }
+            Perevod(dvoid);
+            Console.Write("Это число в двоичной системе счисления: ");
+            for (int i = 0; i < text.Length; i++) Console.Write(text[i]);
+            Console.WriteLine("");
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
