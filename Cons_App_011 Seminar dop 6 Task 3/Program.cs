b20cd7ea0565d41887009e26cd71611286cf3581
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-06 10:42:01 +0300

    Add Seminar dop 6 Task 3

--- Cons_App_011 Seminar dop 6 Task 3/Program.cs
+++ Cons_App_011 Seminar dop 6 Task 3/Program.cs
@@ -1,21 +1,36 @@
-﻿Console.Write("Введите число N: ");
-double a = Convert.ToDouble(Console.ReadLine());
-double n = 3;
+﻿// Задача 3. Дано число N. Используя рекурсию, определите, что оно является степенью числа 3
 
-double stepen = 0;
-double step = 0;
+using System;
 
-double Step(double result)
+namespace ConsoleApp
 {
-    while (a >= n)
+    class Program
     {
-        a = a / 3;
-        stepen = stepen + 1;
-        Console.WriteLine(a + ", " + stepen);
-        step = stepen;
-        Step(a);
-    }
-return step;
-}
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 6 Задача 3 доп =============");
+            Console.Write("Введите число N: ");
+            double number = Convert.ToDouble(Console.ReadLine());
+            double n = 1.0010992159842; // Определим степень с точностью 0,001 = 1/1000 => корень 1000 из 3 = 1.0010992159842
+            double stepen = 0;
+            double result = 0;
 
-Console.WriteLine(Step(a));
+            double Step(double curnumber)
+            {
+                while (number >= 1.001)
+                {
+                    number = number / n;
+                    stepen = stepen + 0.001;
+                    result = stepen;
+                    Step(number);
+                }
+                return result;
+            }
+            Console.WriteLine("Число N = 3 ^" + "{0: 0.001}", Step(number));
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
