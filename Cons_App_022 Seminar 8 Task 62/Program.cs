ecc401f57dca5a0b5ed2f5c53a6471bf0a860460
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-13 15:38:48 +0300

    Add Seminar dop 8 Task 1

--- Cons_App_022 Seminar 8 Task 62/Program.cs
+++ Cons_App_022 Seminar 8 Task 62/Program.cs
@@ -1,83 +1,100 @@
 ﻿// Задача 62: Заполните спирально массив 4 на 4 числами от 1 до 16.
 
-Console.Write("Введите число строк: ");
-int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите число столбцов: ");
-int n = Convert.ToInt32(Console.ReadLine());
+using System;
 
-int[,] spiral = new int[m, n];
-
-int[,] FillArray(int k, int l, int count)
+namespace ConsoleApp
 {
-    if (count <= m * n)
+    class Program
     {
-        int i = k;
-        int j = l;
-        for (j = k; j < n - k; j++)
+        static void Main(string[] args)
         {
-            count++;
-            spiral[i, j] = count;
-        }
-        if (count < m * n)
-        {
-            j--;
-            k = i;
-            l = j;
-            for (i = k + 1; i < m - k; i++)
+            Console.WriteLine("");
+            Console.WriteLine("=============== Семинар 8 Задача 60 =================");
+            Console.Write("Введите число строк: ");
+            int m = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число столбцов: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] spiral = new int[m, n];
+
+            int[,] FillArray(int k, int l, int count)
             {
-            count++;
-            spiral[i, j] = count;
-            }
-            if (count < m * n)
-            {
-                i--;
-                k = i;
-                l = j;
-                for (j = k - 1; j >= m - k - 1; j--)
+                if (count <= m * n)
                 {
-                count++;
-                spiral[i, j] = count;
-                }
-                if (count < m * n)
-                {
-                    j++;
-                    k = i;
-                    l = j;
-                    for (i = k - 1; i > n - k - 1; i--)
+                    int i = k;
+                    int j = l;
+                    for (j = k; j < n - k; j++)
                     {
                         count++;
                         spiral[i, j] = count;
                     }
                     if (count < m * n)
                     {
-                        i++;
+                        j--;
                         k = i;
-                        l = j + 1;
-                        FillArray(k, l, count);
+                        l = j;
+                        for (i = k + 1; i < m - k; i++)
+                        {
+                        count++;
+                        spiral[i, j] = count;
+                        }
+                        if (count < m * n)
+                        {
+                            i--;
+                            k = i;
+                            l = j;
+                            for (j = k - 1; j >= m - k - 1; j--)
+                            {
+                            count++;
+                            spiral[i, j] = count;
+                            }
+                            if (count < m * n)
+                            {
+                                j++;
+                                k = i;
+                                l = j;
+                                for (i = k - 1; i > n - k - 1; i--)
+                                {
+                                    count++;
+                                    spiral[i, j] = count;
+                                }
+                                if (count < m * n)
+                                {
+                                    i++;
+                                    k = i;
+                                    l = j + 1;
+                                    FillArray(k, l, count);
+                                }
+                            }
+                        }
                     }
                 }
+                return spiral;
             }
+
+            int[,] spiralArray = new int[m, n];
+            spiralArray = FillArray(0, 0, 0);
+
+            void Print(int[,] arr)
+            {
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(arr[i, j] + "  ");
+                    }
+                    Console.WriteLine("");
+                }
+                Console.WriteLine("----------------------");
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Массив " + m + " x " + n + " заполненный спирально числами от 1 до " + m * n);
+            Console.WriteLine("----------------------");
+            Print(spiralArray);
+            Console.WriteLine("");
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
         }
     }
-    return spiral;
 }
-
-int[,] spiralArray = new int[m, n];
-spiralArray = FillArray(0, 0, 0);
-
-void Print(int[,] arr)
-{
-    for (int i = 0; i < m; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            Console.Write(arr[i, j] + "  ");
-        }
-        Console.WriteLine("");
-    }
-    Console.WriteLine("----------------------");
-}
-Console.WriteLine("");
-Console.WriteLine("Массив " + m + " x " + n + " заполненный спирально числами от 1 до " + m * n);
-Console.WriteLine("----------------------");
-Print(spiralArray);
---
+++ Cons_App_023 Seminar dop 8 Task 1/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задача 1. Дан двумерный массив.
+// Заменить в нём элементы первой строки элементами главной диагонали.
+// А элементы последней строки, элементами побочной диагонали.
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 8 Задача 1 доп =============");
+            Console.Write("Введите размер массива: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] startArray = new int[n, n];
+            Random rand = new Random();
+            Console.WriteLine("Исходный массив: ");
+            Console.WriteLine("------------------------");
+            void Fill(int[,] arr)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        startArray[i, j] = rand.Next(10, 100);
+                    }
+                }
+            }
+            void Print(int[,] arr)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        Console.Write(arr[i, j] + " ");
+                    }
+                    Console.WriteLine("");
+                }
+            }
+
+            Fill(startArray);
+            Print(startArray);
+            Console.WriteLine("------------------------");
+            int temp = startArray[0, n - 1];
+            for (int i = 0; i < n; i++)
+            {
+                int j = i;
+                startArray[0, j] = startArray[i, j];
+                int k = n - 1 - i;
+                startArray[n - 1, i] = startArray[k, i];
+            }
+            Console.WriteLine("Отсортированный массив: ");
+            Console.WriteLine("------------------------");
+            startArray[n - 1, n - 1] = temp;
+            Print(startArray);
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
