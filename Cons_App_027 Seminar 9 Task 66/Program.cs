17c65bfeba70243cc457a7f8d32885aaaca87b30
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-14 20:37:10 +0300

    Add Seminar 9 Task 66

---
+++ Cons_App_027 Seminar 9 Task 66/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.Write("Введите начальное число: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное число: ");
+int finish = Convert.ToInt32(Console.ReadLine());
+
+int startTemp =start;
+int summa = 0;
+while (start <= finish)
+{
+        summa = summa + start;
+        start ++;
+}
+Console.WriteLine("M = " + startTemp + "; N = " + finish + ". -> " + summa);
