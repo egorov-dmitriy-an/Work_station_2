a7822292ebb333d6a8cbe8a2b51aa48fdd9de10b
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-21 23:56:39 +0300

    Add edit

---
+++ Cons_App_036 Seminar dop 10 Task 3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 3. Дан массив, состоящий из случайных целых чисел.
+// Дано число M. Выведите случайное подмножество массива,
+// сумма элементов в котором не превосходит M.
+// {11, 52, 36, 8, 19}, M = 50 >> 11, 19, 8
+// {11, 52, 36, 8, 19}, M = 50 >> 36, 8
+
+Console.Write("Введите размер массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+Random rand = new Random();
+int [] array = new int[length];
+Console.Write("");
+
+for (int i = 0; i < length; i++)
+{
+    array[i] = rand.Next(0, 100);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine("");
+
+Console.Write("Введите число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
