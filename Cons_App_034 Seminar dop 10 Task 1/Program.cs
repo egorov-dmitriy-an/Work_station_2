44f0bbce8a2fe4e5c81c1087db8d9a9512fc359f
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-21 15:59:53 +0300

    Add Seminar dop 10 Task 1

---
+++ Cons_App_034 Seminar dop 10 Task 1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 1. Даны два числа a, b.
+// Сложите их, используя только операции инкремента и декремента.
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("================ Семинар 10 Задача 1 доп ============");
+            Console.Write("Введите число a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < b; i++) a++;
+            Console.WriteLine("Сумма a+b равна: " + a);
+            Console.WriteLine("=====================================================");
+            Console.WriteLine("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
