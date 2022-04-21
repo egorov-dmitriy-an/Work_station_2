4f6e0ce13b57b852aebcf3bf0ce7be3d3225a68c
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-04-21 15:16:07 +0300

    Add Seminar 11 Task 3

---
+++ Cons_App_033 Seminar 11 Task 3/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задача 3. Напишите рекурсивный метод, который принимает
+// номер года и определяет, является ли он високосным или нет.
+
+using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("=============== Семинар 11 Задача 3 =================");
+            void Year(int numb)
+            {
+                if (numb < 4)
+                {
+                    if (numb == 0) Console.WriteLine("Этот год високосный.");
+                    else Console.WriteLine("Этот год невисокосный.");
+                    return;
+                }
+                else 
+                {
+                    numb -= 4;
+                    Year(numb);
+                }
+            }
+            Console.Write("Введите номер года: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Year(number);
+            Console.WriteLine("=====================================================");
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
