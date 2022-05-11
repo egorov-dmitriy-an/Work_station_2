675f318a19189df2f9fb42a037f9de8a800127a6
egorov.dmitriy.an <egorov.dmitriy.an@yandex.ru>
2022-05-11 14:07:57 +0300

    Add readme

--- Cons_App_037 Control work/Program.cs
+++ Cons_App_037 Control work/Program.cs
@@ -1,54 +1,65 @@
-﻿Console.WriteLine();
-Console.WriteLine("-------------Контрольная работа--------------");
-Console.Write("Введите количество строк исходного массива: "); // Зададим количество элементов массива.
-int length = Convert.ToInt32(Console.ReadLine());
+﻿using System;
 
-string[] textArray = new string[length]; // Инициализируем массив и заполним его.
-for (int i = 0; i < length; i++)
+namespace ConsoleApp
 {
-    Console.Write("Строка с индексом " + i + ": ");
-    textArray[i] = Console.ReadLine();
-}
-
-Console.WriteLine("---------------------------------------------");
-
-Console.Write("Исходный массив: ");
-Console.Write("[");
-for (int l = 0; l < length; l++)
-{
-    Console.Write("'" + textArray[l] + "'");
-    if (l != length - 1)
+    class Program
     {
-        Console.Write(", ");
-    }
-}
-Console.WriteLine("]");
-
-int newLength = 0; // Определим количество строк, длина которых менее или равна 3.
-for (int j = 0; j < length; j++)
-{
-    if (textArray[j].Length <= 3) newLength++;
-}
-
-Console.WriteLine("---------------------------------------------");
-
-Console.Write("Новый массив: ");
-int count = 0;
-string[] newTextArray = new string[newLength];
-Console.Write("[");
-for (int k = 0; k < length; k++)
-{
-    if (textArray[k].Length <= 3)
-    {
-        newTextArray[count] = textArray[k];
-        Console.Write("'" + newTextArray[count] + "'");
-        if (count != newLength - 1)
+        static void Main(string[] args)
         {
-            Console.Write(", ");
+            Console.WriteLine();
+            Console.WriteLine("-------------Контрольная работа--------------");
+
+            // Предложим задать количество элементов массива.
+            Console.Write("Введите количество строк исходного массива: ");
+            int length = Convert.ToInt32(Console.ReadLine());
+
+            // Инициализируем массив и предложим заполнить его строками.
+            string[] textArray = new string[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("Введите строку с индексом " + i + ": ");
+                textArray[i] = Console.ReadLine();
+            }
+            Console.WriteLine("---------------------------------------------");
+
+            // Выведем полученный массив.
+            Console.Write("Исходный массив: ");
+            Console.Write("[");
+            for (int l = 0; l < length; l++)
+            {
+                Console.Write($"'{textArray[l]}'");
+                if (l != length - 1) Console.Write(", ");
+            }
+            Console.WriteLine("]");
+
+            // Определим количество строк, длина которых менее или равна 3.
+            int newLength = 0;
+            for (int j = 0; j < length; j++)
+            {
+                if (textArray[j].Length <= 3) newLength++;
+            }
+            Console.WriteLine("---------------------------------------------");
+            Console.Write("Новый массив: ");
+
+            // Инициализируем новый массив.
+            int count = 0;
+            string[] newTextArray = new string[newLength];
+            Console.Write("[");
+
+            // Заполним массив строками, длина которых менее или равна 3 и выведем его.
+            for (int k = 0; k < length; k++)
+            {
+                if (textArray[k].Length <= 3)
+                {
+                    newTextArray[count] = textArray[k];
+                    Console.Write($"'{newTextArray[count]}'");
+                    if (count != newLength - 1) Console.Write(", ");
+                    count++;
+                }
+            }
+            Console.WriteLine("]");
+            Console.WriteLine("-------------------Конец---------------------");
+            Console.WriteLine();
         }
-        count++;
     }
 }
-Console.WriteLine("]");
-Console.WriteLine("-------------------Конец---------------------");
-Console.WriteLine();
